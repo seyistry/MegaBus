e46460cde4d6379313d45e36c8021b5a672cf779
seyi egunjobi <seyistry@gmail.com>
2021-08-16 23:17:13 +0100

    improve Input form

---
+++ components/input/styles.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        width: "100%",
+        height: 58,
+        backgroundColor: "#FAFAFC",
+        borderWidth: 1,
+        marginTop: 20,
+        borderColor: "#E2E9F8",
+        justifyContent: "center",
+        borderRadius: 10,
+    },
+    text: {
+        fontSize: 16,
+        color: "black",
+        marginLeft: 10,
+    },
+    icon: {
+        marginRight: 10,
+        justifyContent: "center",
+    },
+    input: {
+        marginLeft: 10,
+        color: "#21293B",
+        width: 280,
+    },
+});
+
+export default styles;
