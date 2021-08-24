bc5825801edd0a0d34b72e6e85633f7ee832eefc
seyi egunjobi <seyistry@gmail.com>
2021-08-24 23:33:16 +0100

    lock screen orientation to PORTRAIT_UP

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import Constants from "expo-constants";
+import * as ScreenOrientation from "expo-screen-orientation";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import LoadApp from "./screens/LoadApp";
 import Onboarding from "./screens/onboarding/Onboarding";
 import LoginSignUp from "./screens/LoginSignUp";
 import Login from "./screens/Login";
+import SignUp from "./screens/signup/SignUp";
 import { pryColor } from "./utils/color";
 
 function MegaStatusBar({ backgroundColor, ...props }) {
@@ -24,10 +26,19 @@ function MegaStatusBar({ backgroundColor, ...props }) {
 }
 
 export default function App() {
+    useEffect(() => {
+        lockOrientation();
+    }, []);
+
+    const lockOrientation = async () => {
+        await ScreenOrientation.lockAsync(
+            ScreenOrientation.OrientationLock.PORTRAIT_UP
+        );
+    };
     return (
         <View style={styles.container}>
             <MegaStatusBar backgroundColor={pryColor} style="light" />
-            <Login />
+            <SignUp />
         </View>
     );
 }
