0cca3f010601cd45beb711e46edbd7395294dc42
seyi egunjobi <seyistry@gmail.com>
2021-09-07 10:34:13 +0100

    add blue, gray, green and black color

--- utils/color.js
+++ utils/color.js
@@ -1,3 +1,7 @@
 export const pryColor = "#189AB4";
 export const GradientColor = "#05445E";
 export const white = "#FFFFFF";
+export const blue = "#05445E";
+export const gray = "#637E8D";
+export const green = "#029888";
+export const black = "#364852";
