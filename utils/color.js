35e9ff887d7904704bbe70e2bf013628763b1748
seyi egunjobi <seyistry@gmail.com>
2021-10-31 13:10:12 +0100

    add grey color

--- utils/color.js
+++ utils/color.js
@@ -3,5 +3,6 @@ export const GradientColor = "#05445E";
 export const white = "#FFFFFF";
 export const blue = "#05445E";
 export const gray = "#637E8D";
+export const grey = "#ADADAD";
 export const green = "#029888";
 export const black = "#364852";
