ee501d7d36abc70743e6b85d0d80e5ec2173544e
seyi egunjobi <seyistry@gmail.com>
2021-10-31 13:03:57 +0100

    add onPress method for toggle button

--- components/button/pryButton/PButton.js
+++ components/button/pryButton/PButton.js
@@ -3,7 +3,7 @@ import { Text, TouchableOpacity, View } from "react-native";
 import styles from './styles.js'
 
 const PButton = (props) => (
-    <TouchableOpacity style={styles.buttonContainer}>
+    <TouchableOpacity style={styles.buttonContainer} onPress={props.method}>
         <Text style={styles.text}>{props.name}</Text>
     </TouchableOpacity>
 );
