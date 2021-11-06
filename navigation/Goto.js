c99c519222e238774d1a8dd053b4246b0a866887
seyi egunjobi <seyistry@gmail.com>
2021-11-06 17:59:59 +0100

    add universal function to control navigation around the app

---
+++ navigation/Goto.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+const Goto = (props) => {
+    const navigation = useNavigation();
+    const { direction, title, description } = props;
+    console.log(direction)
+
+    const handleGoto = () => {
+        navigation.navigate(direction, {
+            title: title,
+            description: description,
+        });
+    };
+    return handleGoto;
+};
+
+export default Goto;
