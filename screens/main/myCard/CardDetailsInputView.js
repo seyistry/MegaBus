1d1707473c2cc08e4f77105e1af15d8f5983e773
seyi egunjobi <seyistry@gmail.com>
2021-11-06 18:05:43 +0100

    add user card input screen

---
+++ screens/main/myCard/CardDetailsInputView.js
@@ -0,0 +1,231 @@
+import React, { useState } from "react";
+import {
+    Text,
+    View,
+    StyleSheet,
+    TextInput,
+    Button,
+    Alert,
+    ScrollView,
+} from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import { useForm, Controller } from "react-hook-form";
+import {
+    cardNumberFormatter,
+    expirationDateFormatter,
+} from "../../../utils/formatters";
+import Header from "../../../components/Header";
+import { pryColor } from "../../../utils/color";
+import PButton from "../../../components/button/pryButton/PButton";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Goto from "../../../navigation/Goto";
+
+const Pickers = () => {
+    const [selectedOptions, setSelectedOptions] = useState();
+    return (
+        <View style={styles.input}>
+            <Picker
+                selectedValue={selectedOptions}
+                onValueChange={(itemValue, itemIndex) =>
+                    setSelectedOptions(itemValue)
+                }
+            >
+                <Picker.Item
+                    label="Select Bank"
+                    value=""
+                    style={{
+                        // marginLeft: 100,
+                        color: pryColor,
+                        fontSize: 12,
+                    }}
+                />
+                <Picker.Item
+                    label="BOA"
+                    value="BOA"
+                    style={styles.pickerItem}
+                />
+                <Picker.Item
+                    label="Chase"
+                    value="Chase"
+                    style={styles.pickerItem}
+                />
+                <Picker.Item label="JP" value="JP" style={styles.pickerItem} />
+            </Picker>
+        </View>
+    );
+};
+
+const CardDetailsInputView = () => {
+    const {
+        register,
+        setValue,
+        handleSubmit,
+        control,
+        reset,
+        formState: { errors },
+    } = useForm({
+        defaultValues: {
+            firstName: "",
+            lastName: "",
+        },
+    });
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+
+    const handlePress = () => {
+        return Goto({ direction: "MyCardOTPcodeView" });
+    };
+
+    const onChange = (arg) => {
+        return {
+            value: arg.nativeEvent.text,
+        };
+    };
+
+    // console.log("errors", errors);
+
+    const handleChange = (value) => {
+        cardNumberFormatter;
+    };
+
+    return (
+        <View style={styles.container}>
+            <Header name="My Card" />
+            <View style={{ marginHorizontal: 20, marginTop: 20 }}>
+                <Text style={{ fontFamily: "HeeboXb", fontSize: 14 }}>
+                    Add New Bank Card
+                </Text>
+                <Text
+                    style={{
+                        fontFamily: "HeeboR",
+                        fontSize: 14,
+                        marginTop: 5,
+                    }}
+                >
+                    Please fill in the card information
+                </Text>
+            </View>
+            <View style={{ flex: 1, marginHorizontal: 20, marginTop: 20 }}>
+                <Text style={styles.label}>Select Bank</Text>
+                <Pickers />
+                <Text style={styles.label}>Card Number</Text>
+                <Controller
+                    control={control}
+                    render={({ field: { onChange, onBlur, value } }) => (
+                        <TextInput
+                            keyboardType="numeric"
+                            placeholder="xxxx-xxxx-xxxx-xxxx"
+                            style={styles.input}
+                            onBlur={onBlur}
+                            onChangeText={(newValue) =>
+                                onChange(cardNumberFormatter(value, newValue))
+                            }
+                            value={value}
+                            maxLength={19}
+                        />
+                    )}
+                    name="firstName"
+                    rules={{ required: true }}
+                />
+                <View style={{ flexDirection: "row" }}>
+                    <View style={{ width: "60%" }}>
+                        <Text style={styles.label}>Exp. Date</Text>
+                        <Controller
+                            control={control}
+                            render={({
+                                field: { onChange, onBlur, value },
+                            }) => (
+                                <TextInput
+                                    keyboardType="numeric"
+                                    placeholder="MM/YY"
+                                    style={styles.input}
+                                    onBlur={onBlur}
+                                    onChangeText={(newValue) =>
+                                        onChange(
+                                            expirationDateFormatter(
+                                                value,
+                                                newValue
+                                            )
+                                        )
+                                    }
+                                    value={value}
+                                    maxLength={5}
+                                />
+                            )}
+                            name="lastName"
+                            rules={{ required: true }}
+                        />
+                    </View>
+                    <View style={{ width: "40%", paddingLeft: 20 }}>
+                        <Text style={styles.label}>CVV</Text>
+                        <Controller
+                            control={control}
+                            render={({
+                                field: { onChange, onBlur, value },
+                            }) => (
+                                <TextInput
+                                    keyboardType="numeric"
+                                    style={styles.input}
+                                    onBlur={onBlur}
+                                    onChangeText={(value) => onChange(value)}
+                                    value={value}
+                                    maxLength={3}
+                                />
+                            )}
+                            name="lastName"
+                            rules={{ required: true }}
+                        />
+                    </View>
+                </View>
+
+                <View style={styles.button}>
+                    <TouchableOpacity onPress={handlePress()}>
+                        <PButton name="Next" />
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    );
+};
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#ffffff",
+    },
+    label: {
+        color: "#8F8F8F",
+        margin: 10,
+        marginLeft: 0,
+    },
+    button: {
+        flex: 1,
+        // justifyContent: "flex-end",
+        marginTop: 180,
+        alignItems: "center",
+        borderRadius: 4,
+    },
+    input: {
+        backgroundColor: "white",
+        borderColor: pryColor,
+        borderWidth: 1,
+        height: 44,
+        padding: 10,
+        borderRadius: 4,
+    },
+    pickerItem: {
+        fontSize: 14,
+        color: pryColor,
+    },
+    optionsContainer: {
+        // width: 110,
+        // borderWidth: 1,
+        // borderColor: "#C2DEEE",
+        // height: 28,
+        // borderRadius: 50,
+        // justifyContent: "center",
+    },
+});
+
+export default CardDetailsInputView;
