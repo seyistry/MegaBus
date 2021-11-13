d1582d4fd073a557f80eb6b3a5cadb278671c95a
seyi egunjobi <seyistry@gmail.com>
2021-11-13 16:21:59 +0100

    improve Scan style

--- screens/main/scanToPay/Scan.js
+++ screens/main/scanToPay/Scan.js
@@ -1,13 +1,33 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, Animated } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
 import { useNavigation } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
 import { pryColor } from "../../../utils/color";
 
 const Scan = () => {
     const navigation = useNavigation();
     const [hasPermission, setHasPermission] = useState(null);
     const [scanned, setScanned] = useState(false);
+    const [animationLineHeight, setAnimationLineHeight] = useState(0);
+    const [focusLineAnimation, setFocusLineAnimation] = useState(
+        new Animated.Value(0)
+    );
+
+    const animateLine = () => {
+        Animated.sequence([
+            Animated.timing(focusLineAnimation, {
+                toValue: 1,
+                duration: 1000,
+                useNativeDriver: true,
+            }),
+            Animated.timing(focusLineAnimation, {
+                toValue: 0,
+                duration: 1000,
+                useNativeDriver: true,
+            }),
+        ]).start(animateLine);
+    };
 
     const handleScanRedirect = (props) => {
         navigation.navigate("ScanToPayMainView", {
@@ -22,6 +42,7 @@ const Scan = () => {
             const { status } = await BarCodeScanner.requestPermissionsAsync();
             setHasPermission(status === "granted");
         })();
+        animateLine();
     }, []);
 
     const handleBarCodeScanned = ({ type, data }) => {
@@ -47,12 +68,56 @@ const Scan = () => {
                 onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
                 style={StyleSheet.absoluteFillObject}
             />
-            {scanned && (
-                <Button
-                    title={"Tap to Scan Again"}
-                    onPress={() => setScanned(false)}
-                />
-            )}
+            <View style={styles.overlay}>
+                <View style={styles.unfocusedContainer}></View>
+                <View style={styles.middleContainer}>
+                    <View style={styles.unfocusedContainer} />
+                    <View
+                        onLayout={(e) =>
+                            setAnimationLineHeight(e.nativeEvent.layout.height)
+                        }
+                        style={styles.focusedContainer}
+                    >
+                        {!scanned && (
+                            <Animated.View
+                                style={[
+                                    styles.animationLineStyle,
+                                    {
+                                        transform: [
+                                            {
+                                                translateY:
+                                                    focusLineAnimation.interpolate(
+                                                        {
+                                                            inputRange: [0, 1],
+                                                            outputRange: [
+                                                                0,
+                                                                animationLineHeight,
+                                                            ],
+                                                        }
+                                                    ),
+                                            },
+                                        ],
+                                    },
+                                ]}
+                            />
+                        )}
+                        {scanned && (
+                            <TouchableOpacity
+                                onPress={() => setScanned(false)}
+                                style={styles.rescanIconContainer}
+                            >
+                                <Ionicons
+                                    name="reload"
+                                    size={50}
+                                    color={pryColor}
+                                />
+                            </TouchableOpacity>
+                        )}
+                    </View>
+                    <View style={styles.unfocusedContainer} />
+                </View>
+                <View style={styles.unfocusedContainer} />
+            </View>
         </View>
     );
 };
@@ -60,9 +125,35 @@ const Scan = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: "column",
-        justifyContent: "center",
         backgroundColor: pryColor,
+    },
+    overlay: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+    },
+    unfocusedContainer: {
+        flex: 1,
+        backgroundColor: "rgba(0,0,0,0.7)",
+    },
+    middleContainer: {
+        flexDirection: "row",
+        flex: 1.5,
+    },
+    focusedContainer: {
+        flex: 6,
+    },
+    animationLineStyle: {
+        height: 2,
+        width: "100%",
+        backgroundColor: "red",
+    },
+    rescanIconContainer: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
     },
 });
 
