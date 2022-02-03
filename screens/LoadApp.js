17d60b30b9ef62e13a74d374f3a4514abbe90ecf
Seyi Egunjobi <seyistry@gmail.com>
2022-02-03 22:33:22 +0100

    remove unused module

--- screens/LoadApp.js
+++ screens/LoadApp.js
@@ -1,12 +1,7 @@
-import React, { useRef, useEffect, useState } from "react";
-import { Text, View, StyleSheet, Image, Animated } from "react-native";
-// import { NavigationContainer } from "@react-navigation/native";
-// import Drawers from "../navigation/Drawers";
+import React, { useRef, useEffect } from "react";
+import { View, Animated } from "react-native";
 import { pryColor } from "../utils/color";
 import Logo from "../assets/image/Logo";
-// import { auth } from "../firebase/firebase.utils";
-// import { onAuthStateChanged } from "firebase/auth";
-// import { preAuthStacks } from "../navigation/Stacks";
 
 const FadeInView = (props) => {
     const fadeAnim = useRef(new Animated.Value(0)).current; // Initial value for opacity: 0
@@ -37,58 +32,6 @@ const FadeInView = (props) => {
         </Animated.View>
     );
 };
-
-// const LoadApp = () => {
-//     const [routeState, setRouteState] = useState(false);
-//     // const [User, setUser] = useState(null);
-//     useEffect(() => {
-//         onAuthStateChanged(auth, (user) => {
-//             if (routeState) {
-//                 setRouteState(true);
-//                 console.log(`${routeState} A`);
-//             } else if (!user && !routeState) {
-//                 // setRouteState(false);
-//                 console.log(`${routeState} B`);
-//             } else {
-//                 setRouteState(true);
-//                 console.log(`${routeState} C`);
-//             }
-//             setTimeout(() => {
-//                 // if (user) {
-//                 //     console.log(user.uid);
-//                     <NavigationContainer>
-//                         <Drawers />
-//                     </NavigationContainer>;
-//                 // } else {
-//                 //     // <NavigationContainer>
-//                 //         <preAuthStacks />
-
-//                 // }
-//             }, 1500);
-//         });
-//     }, [routeState]);
-
-//     return (
-//         <View
-//             style={{
-//                 flex: 1,
-//                 backgroundColor: pryColor,
-//             }}
-//         >
-//             <View
-//                 style={{
-//                     flex: 1,
-//                     justifyContent: "center",
-//                     alignItems: "center",
-//                 }}
-//             >
-//                 <FadeInView>
-//                     <Logo />
-//                 </FadeInView>
-//             </View>
-//         </View>
-//     );
-// };
 
 const LoadApp = () => {
     return (
