c1ac5edbaa26186cf5fc426c9afb448a7dc76e59
Seyi Egunjobi <seyistry@gmail.com>
2022-02-03 22:31:42 +0100

    remove LoadApp from navigation container

--- screens/LoadApp.js
+++ screens/LoadApp.js
@@ -1,9 +1,12 @@
 import React, { useRef, useEffect, useState } from "react";
 import { Text, View, StyleSheet, Image, Animated } from "react-native";
+// import { NavigationContainer } from "@react-navigation/native";
+// import Drawers from "../navigation/Drawers";
 import { pryColor } from "../utils/color";
 import Logo from "../assets/image/Logo";
-import { auth } from "../firebase/firebase.utils";
-import { onAuthStateChanged } from "firebase/auth";
+// import { auth } from "../firebase/firebase.utils";
+// import { onAuthStateChanged } from "firebase/auth";
+// import { preAuthStacks } from "../navigation/Stacks";
 
 const FadeInView = (props) => {
     const fadeAnim = useRef(new Animated.Value(0)).current; // Initial value for opacity: 0
@@ -35,21 +38,59 @@ const FadeInView = (props) => {
     );
 };
 
-const LoadApp = ({ navigation }) => {
-    const [routeState, setRouteState] = useState(false);
-    useEffect(() => {
-        onAuthStateChanged(auth, (user) => {
-            setRouteState(true);
-            setTimeout(() => {
-                user
-                    ? navigation.navigate("Main")
-                    : navigation.navigate(
-                          `${routeState ? "Onboarding" : "Login"}`
-                      );
-            }, 1500);
-        });
-    }, []);
+// const LoadApp = () => {
+//     const [routeState, setRouteState] = useState(false);
+//     // const [User, setUser] = useState(null);
+//     useEffect(() => {
+//         onAuthStateChanged(auth, (user) => {
+//             if (routeState) {
+//                 setRouteState(true);
+//                 console.log(`${routeState} A`);
+//             } else if (!user && !routeState) {
+//                 // setRouteState(false);
+//                 console.log(`${routeState} B`);
+//             } else {
+//                 setRouteState(true);
+//                 console.log(`${routeState} C`);
+//             }
+//             setTimeout(() => {
+//                 // if (user) {
+//                 //     console.log(user.uid);
+//                     <NavigationContainer>
+//                         <Drawers />
+//                     </NavigationContainer>;
+//                 // } else {
+//                 //     // <NavigationContainer>
+//                 //         <preAuthStacks />
 
+//                 // }
+//             }, 1500);
+//         });
+//     }, [routeState]);
+
+//     return (
+//         <View
+//             style={{
+//                 flex: 1,
+//                 backgroundColor: pryColor,
+//             }}
+//         >
+//             <View
+//                 style={{
+//                     flex: 1,
+//                     justifyContent: "center",
+//                     alignItems: "center",
+//                 }}
+//             >
+//                 <FadeInView>
+//                     <Logo />
+//                 </FadeInView>
+//             </View>
+//         </View>
+//     );
+// };
+
+const LoadApp = () => {
     return (
         <View
             style={{
